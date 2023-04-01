5e76aa741fbf955b2538c0a5ef259aa94c752eae
Alex Batts <alex.batts05@gmail.com>
2023-04-01 14:34:10 -0400

    Upgraded from legacy components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SearchPaneComponent } from './search-pane/search-pane.component';
-import { MatLegacyCardModule as MatCardModule } from '@angular/material/legacy-card';
-import { MatLegacyInputModule as MatInputModule } from '@angular/material/legacy-input';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { MatLegacySelectModule as MatSelectModule } from '@angular/material/legacy-select';
-import { MatLegacyAutocompleteModule as MatAutocompleteModule } from '@angular/material/legacy-autocomplete';
+import { MatSelectModule } from '@angular/material/select';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { ReactiveFormsModule } from '@angular/forms';
+import { FeedbackFormComponent } from './feedback-form/feedback-form.component';
 
 @NgModule({
-  declarations: [AppComponent, SearchPaneComponent],
+  declarations: [AppComponent, SearchPaneComponent, FeedbackFormComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
